ae92c6098b638fa08bbc3f4699dbb7f2233e5d63
Michael Kühnel <m.kuehnel@micromata.de>
2018-03-28 17:02:20 +0200

    Setup basic routing with react-router

--- build/webpack/config.dev-server.js
+++ build/webpack/config.dev-server.js
@@ -6,5 +6,6 @@ import {isDevMode} from './helpers';
 export const devServer = {
 	contentBase: isDevMode() ? path.join(__dirname, '../', mainDirectories.dev) : path.join(__dirname, '../', mainDirectories.prod),
 	port: isDevMode() ? 3000 : 3001,
-	overlay: true
+	overlay: true,
+	historyApiFallback: true
 };
--- src/app/index.js
+++ src/app/index.js
@@ -6,6 +6,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import {Button} from 'reactstrap';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
 // Import polyfills
 import {applyPolyfills} from './base/polyfills';
@@ -23,25 +24,84 @@ import '../assets/scss/index.scss';
 	consoleErrorFix();
 	ieViewportFix();
 
-	function HelloPlanet(props) {
-		return (
-			<React.Fragment>
-				<p>Hello {props.planet || 'World'}!</p>
-				<Button color="primary">Okay</Button>
-			</React.Fragment>
-		);
-	}
+	const Home = () => (
+		<div>
+			<h2>Home</h2>
+		</div>
+	);
+
+	const HelloPlanet = (props) => (
+		<React.Fragment>
+			<p>Hello {props.planet || 'World'}!</p>
+			<Button color="primary">Okay</Button>
+		</React.Fragment>
+	);
 
 	HelloPlanet.propTypes = {
 		planet: PropTypes.string
 	};
 
-	ReactDOM.render(
-		<HelloPlanet planet="Mars" />,
-
-		document.getElementById('root')
+	const Topic = ({match}) => (
+		<div>
+			<h3>{match.params.topicId}</h3>
+		</div>
 	);
 
+	Topic.propTypes = {
+		match: PropTypes.object
+	};
+
+	const Topics = ({match}) => (
+		<div>
+			<h2>Topics</h2>
+			<ul>
+				<li>
+					<Link to={`${match.url}/rendering`}>Rendering with React</Link>
+				</li>
+				<li>
+					<Link to={`${match.url}/components`}>Components</Link>
+				</li>
+				<li>
+					<Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+				</li>
+			</ul>
+
+			<Route path={`${match.path}/:topicId`} component={Topic} />
+			<Route exact path={match.path} render={() => <h3>Please select a topic.</h3>} />
+		</div>
+	);
+
+	Topics.propTypes = {
+		match: PropTypes.object
+	};
+
+	const App = () => (
+		<Router>
+			<div>
+				<ul>
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+					<li>
+						<Link to="/hello-planet">Hello Planet</Link>
+					</li>
+					<li>
+						<Link to="/topics">Topics</Link>
+					</li>
+				</ul>
+
+				<hr />
+
+				<Route exact path="/" component={Home} />
+				<Route path="/hello-planet" render={(props) => <HelloPlanet {...props} planet="Mars" />} />
+				<Route path="/topics" component={Topics} />
+			</div>
+		</Router>
+	);
+
+	ReactDOM.render(
+		<App />,
+		document.getElementById('root'));
 })().catch(err => {
 	console.error(err);
 });
