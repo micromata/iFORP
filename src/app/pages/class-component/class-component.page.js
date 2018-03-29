0cfb29998fc611b2dc683c78041a7a66ad7db262
Michael Kühnel <m.kuehnel@micromata.de>
2018-03-29 17:10:22 +0200

    Refactor class component

--- src/app/pages/class-component/class-component.page.js
+++ src/app/pages/class-component/class-component.page.js
@@ -1,31 +1,9 @@
 import React from 'react';
-import {Button} from 'reactstrap';
+import {Counter} from './counter';
 
-class ClassComponent extends React.Component {
-	state = {count: 0}
-
-	decrementIsDisabled = () => this.state.count === 0;
-
-	decrement = () => {
-		this.setState(state => ({count: state.count === 0 ? state.count : state.count - 1}));
-	}
-
-	increment = () => {
-		this.setState({count: this.state.count + 1});
-	}
-
-	render() {
-		return (
-			<React.Fragment>
-				<h3>Class Component</h3>
-				<p>Count: {this.state.count}</p>
-				<div className="btn-group">
-					<Button disabled={this.decrementIsDisabled()} color="primary" onClick={this.decrement}>-</Button>
-					<Button color="primary" onClick={this.increment}>+</Button>
-				</div>
-			</React.Fragment>
-		);
-	}
-}
-
-export {ClassComponent};
+export const ClassComponent = () => (
+	<React.Fragment>
+		<Counter />
+		<Counter title="Start with 5" start={5} />
+	</React.Fragment>
+);
---
+++ src/app/pages/class-component/counter.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {Button} from 'reactstrap';
+import {PropTypes} from 'prop-types';
+
+class Counter extends React.Component {
+	state = {count: this.props.start || 0}
+
+	decrementIsDisabled = () => this.state.count === 0;
+
+	decrement = () => {
+		this.setState(state => ({count: state.count === 0 ? state.count : state.count - 1}));
+	}
+
+	increment = () => {
+		this.setState({count: this.state.count + 1});
+	}
+
+	render() {
+		return (
+			<React.Fragment>
+				<h3>{this.props.title || 'Counter'}</h3>
+				<p>Count: {this.state.count}</p>
+				<div className="btn-group mb-4">
+					<Button disabled={this.decrementIsDisabled()} color="primary" onClick={this.decrement}>-</Button>
+					<Button color="primary" onClick={this.increment}>+</Button>
+				</div>
+			</React.Fragment>
+		);
+	}
+}
+
+Counter.propTypes = {
+	title: PropTypes.string,
+	start: PropTypes.number
+};
+
+export {Counter};
