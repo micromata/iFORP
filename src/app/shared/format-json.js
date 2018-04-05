0cb347409774602426e8fd1b0c2746c1e1d6a5c0
Michael Kühnel <m.kuehnel@micromata.de>
2018-04-05 22:03:26 +0200

    Add FormatJson component

---
+++ src/app/shared/format-json.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const FormatJson = ({json}) => (
+	<pre className="pre-scrollable bg-light p-3">
+		<code>{JSON.stringify(json, null, 2)}</code>
+	</pre>
+);
+
+FormatJson.propTypes = {
+	json: PropTypes.object || PropTypes.array
+};
