59c497d3f707bcfae83682c043043426e27b7941
Michael Kühnel <m.kuehnel@micromata.de>
2018-04-10 15:52:14 +0200

    Add dropdown menu to switch between whiteboards

--- frontend/src/app/pages/whiteboards/whiteboards.header.js
+++ frontend/src/app/pages/whiteboards/whiteboards.header.js
@@ -1,19 +1,61 @@
 import React from 'react';
+import {Dropdown, DropdownToggle, DropdownMenu, DropdownItem} from 'reactstrap';
 import {PropTypes} from 'prop-types';
+import {Link} from 'react-router-dom';
 
-export const Header = ({project, currentWhiteboard, whiteboards}) => (
-	<div className="row text-center mb-4">
-		<div className="col-2 text-left align-self-center">
-			<a href="start.html"><span className="oi oi-x"></span></a>
-		</div>
-		<div className="col-8">
-			<h1 className="h2">{project.name} / {currentWhiteboard.name}</h1>
-		</div>
-		<div className="col-2 text-right align-self-center">
-			<a href="start.html"><span className="oi oi-menu"></span></a>
-		</div>
-	</div>
-);
+export class Header extends React.Component {
+	toggle = this.toggle.bind(this);
+
+	state = {
+		dropdownOpen: false
+	};
+
+	toggle() {
+		this.setState({dropdownOpen: !this.state.dropdownOpen});
+	}
+
+	render() {
+		const {project, currentWhiteboard, whiteboards} = this.props;
+		return (
+			<div className="row text-center mb-4">
+				<div className="col-2 text-left align-self-center">
+					<Link to="/"><span className="oi oi-x"></span></Link>
+				</div>
+				<div className="col-8">
+					<h1 className="h2">{project.name} / {currentWhiteboard.name}</h1>
+				</div>
+				<div className="col-2 text-right align-self-center">
+					{/* <a href="#"><span className="oi oi-menu"></span></a> */}
+					<Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle} direction="down">
+						<DropdownToggle
+							tag="a"
+							onClick={this.toggle}
+							aria-expanded={this.state.dropdownOpen}
+						>
+							<span className="oi oi-menu"></span>
+						</DropdownToggle>
+						<DropdownMenu>
+							<DropdownItem header>Available whiteboards</DropdownItem>
+							{whiteboards.map(whiteboard => {
+								return <DropdownItem
+									active={whiteboard.id === currentWhiteboard.id}
+									key={whiteboard.id}
+									tag={() => <Link to={`/whiteboards/project-id/${project.id}/whiteboard-id/${whiteboard.id}`} className={`dropdown-item ${whiteboard.id === currentWhiteboard.id && 'active'}`}>{whiteboard.name}</Link>}
+								>
+									{whiteboard.name}
+								</DropdownItem>;
+							})}
+							<DropdownItem divider />
+							<DropdownItem>Create new whiteboard</DropdownItem>
+						</DropdownMenu>
+					</Dropdown>
+				</div>
+			</div>
+		);
+	}
+}
+
+// export const Header = () =>
 
 Header.propTypes = {
 	project: PropTypes.object,
