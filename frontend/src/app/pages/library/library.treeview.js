2a2335fcf781bbd4db415f12ee1dd7c1e6599676
Michael Kühnel <m.kuehnel@micromata.de>
2018-05-07 16:36:41 +0200

    Set the collapsed status by clicking a directory

--- frontend/src/app/pages/library/library.treeview.js
+++ frontend/src/app/pages/library/library.treeview.js
@@ -1,38 +1,64 @@
 import React from 'react';
 import {PropTypes} from 'prop-types';
 
-export const Treeview = ({directories}) => {
+import {FormatJson} from '../../shared/format-json';
 
-	const handleDirectoryClick = (event, directoryId) => {
-		event.preventDefault();
-		console.log('directoryId', directoryId);
+export class Treeview extends React.Component {
+
+	state = {
+		collapsed: {}
 	};
 
-	const handlePageClick = (event, pageId) => {
+	handleDirectoryClick = (event, directoryId) => {
+		event.preventDefault();
+		console.log('directoryId', directoryId);
+		this.setState({
+			collapsed: {
+				...this.state.collapsed,
+				[directoryId]: this.state.collapsed[directoryId] === false
+			}
+		});
+	};
+
+	handlePageClick = (event, pageId) => {
 		event.preventDefault();
 		console.log('pageId', pageId);
 	};
 
-	return (
-		<ul className="list-unstyled">
-			{directories.map((directory) => {
-				return (
-					<li key={directory.id}>
-						<a href="#" onClick={event => handleDirectoryClick(event, directory.id)}>
-							<span className="oi oi-chevron-bottom mr-1"></span>
-							{directory.name}
-						</a>
-						<ul>
-							{directory.pages.map((page) => {
-								return <li key={page.id}><a href="#" onClick={event => handlePageClick(event, page.id)}>{page.name}</a></li>;
-							})}
-						</ul>
-					</li>
-				);
-			})}
-		</ul>
-	);
-};
+	componentDidMount() {
+		const initialCollapsedState = {};
+		this.props.directories.forEach(directory => {
+			initialCollapsedState[directory.id] = true;
+		});
+
+		this.setState({collapsed: initialCollapsedState});
+	}
+
+	render() {
+		return (
+			<React.Fragment>
+				<ul className="list-unstyled">
+					{this.props.directories.map((directory) => {
+						return (
+							<li key={directory.id}>
+								<a href="#" onClick={event => this.handleDirectoryClick(event, directory.id)}>
+									<span className="oi oi-chevron-bottom mr-1"></span>
+									{directory.name}
+								</a>
+								<ul>
+									{directory.pages.map((page) => {
+										return <li key={page.id}><a href="#" onClick={event => this.handlePageClick(event, page.id)}>{page.name}</a></li>;
+									})}
+								</ul>
+							</li>
+						);
+					})}
+				</ul>
+				<FormatJson state={this.state}></FormatJson>
+			</React.Fragment>
+		);
+	}
+}
 
 Treeview.propTypes = {
 	directories: PropTypes.array
