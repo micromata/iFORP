48d318739a51c65116cdc8f4f4118400dca327d6
Michael Kühnel <m.kuehnel@micromata.de>
2018-05-11 12:55:24 +0200

    Make API host configurable via baumeister.json

--- frontend/src/app/base/http.js
+++ frontend/src/app/base/http.js
@@ -1,7 +1,7 @@
 import request from 'axios';
 
-const PRODUCTION_API_PREFIX = 'http://localhost:8087/api/';
-const DEVELOPMENT_API_PREFIX = 'http://localhost:8087/api/';
+const PRODUCTION_API_PREFIX = `http://${API_HOST}:8087/api/`;
+const DEVELOPMENT_API_PREFIX = `http://${API_HOST}:8087/api/`;
 
 // PRODUCTION constant is provided by the webpack DefinePlugin via baumeister.json
 const apiPrefix = PRODUCTION ? PRODUCTION_API_PREFIX : DEVELOPMENT_API_PREFIX;
