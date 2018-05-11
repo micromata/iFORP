0e46c78c36f1ef87ba40a4c920239de0ba9afaa6
Ulrich Schmidt <u.schmidt@micromata.de>
2018-05-11 15:06:36 +0200

    Add thumbnail img

--- frontend/src/app/pages/projects/projects.project-list-item.js
+++ frontend/src/app/pages/projects/projects.project-list-item.js
@@ -2,10 +2,10 @@ import React from 'react';
 import {PropTypes} from 'prop-types';
 import {Link} from 'react-router-dom';
 
-export const ProjectListItem = ({project: {id, thumbnail, name}}) => (
+export const ProjectListItem = ({project: {id, name}}) => (
 	<Link to={`/whiteboards/project/${id}`}>
 		<div className="card m-3">
-			<img className="card-img-top rounded" src={thumbnail} alt="Card image cap" />
+			<img className="card-img-top rounded template-thumb" src="../../assets/img/thumnail.png" alt="Card image cap" />
 			<div className="card-body">
 				<h5 className="card-title">{name}</h5>
 			</div>
