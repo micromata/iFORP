8fbcced7ea860ee21dd384ecc1efebce48693ee4
Michael Kühnel <m.kuehnel@micromata.de>
2018-05-14 21:25:15 +0200

    Setup nested routes for for projects/:projectId/whiteboards

--- backend/src/services/whiteboards/whiteboards.service.js
+++ backend/src/services/whiteboards/whiteboards.service.js
@@ -16,5 +16,27 @@ module.exports = function (app) {
   // Get our initialized service so that we can register hooks and filters
   const service = app.service('whiteboards');
 
+  // re-export the whiteboards service on the /projects/:projectId/whiteboards route
+  app.use('/projects/:projectId/whiteboards', app.service('whiteboards'));
+
+  // A hook that updates `data` with the route parameter
+  function mapProjectIdToData(context) {
+    if(context.data && context.params.route.projectId) {
+      context.data.projectId = context.params.route.projectId;
+    }
+  }
+
+  // For the new route, map the `:projectId` route parameter to the query in a hook
+  app.service('projects/:projectId/whiteboards').hooks({
+    before: {
+      find(context) {
+        context.params.query.projectId = context.params.route.projectId;
+      },
+      create: mapProjectIdToData,
+      update: mapProjectIdToData,
+      patch: mapProjectIdToData
+    }
+  });
+
   service.hooks(hooks);
 };
