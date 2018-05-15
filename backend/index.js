e1a20ca4f6fbb679d271602ddfe3b185d9af4c20
Michael Kühnel <m.kuehnel@micromata.de>
2018-05-15 14:19:46 +0200

    Replace feathers with naked express

---
+++ backend/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello Worldzzzzzzzz!'));
+
+app.listen(3000, () => console.log('Example app listening on http://localhost:3000!'));
