8a8c588a35ac423889cb46f90b74be739c07f74f
Nicolai Süper <n.sueper@micromata.de>
2018-05-15 15:15:07 +0200

    Add initial route method stubs

---
+++ backend/routes/library.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+
+router.get('/files', (req, res) => {
+  // Get all library files
+  res.send('Not implemented!');
+});
+
+router.get('/files/:fileId', (req, res) => {
+  // Get library file by id
+  res.send('Not implemented!');
+});
+
+router.post('/library/upload', (req, res) => {
+  // Save ZIP to library
+  res.send('Not implemented!');
+});
+
+module.exports = router;
---
+++ backend/routes/projects.js
@@ -0,0 +1,73 @@
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+  // Get all projects
+  res.send('Not implemented!');
+});
+
+router.post('/', (req, res) => {
+  // Add project
+  res.send('Not implemented!');
+});
+
+router.get('/:projectId', (req, res) => {
+  // Get project by id
+  res.send('Not implemented!');
+});
+
+router.patch('/:projectId', (req, res) => {
+  // Change project by id
+  res.send('Not implemented!');
+});
+
+router.get('/:projectId/whiteboards', (req, res) => {
+  // Get all whiteboards of project
+  res.send('Not implemented!');
+});
+
+router.post('/:projectId/whiteboards', (req, res) => {
+  // Add whiteboard to project
+  res.send('Not implemented!');
+});
+
+router.delete('/:projectId/whiteboards/:whiteboardId', (req, res) => {
+  // Delete whiteboard from project
+  res.send('Not implemented!');
+});
+
+router.patch('/:projectId/whiteboards/:whiteboardId', (req, res) => {
+  // Change whiteboard
+  res.send('Not implemented!');
+});
+
+router.get('/:projectId/whiteboards/:whiteboardId/views', (req, res) => {
+  // Get all whiteboard views
+  res.send('Not implemented!');
+});
+
+router.post('/:projectId/whiteboards/:whiteboardId/views', (req, res) => {
+  // Add view to whiteboard
+  res.send('Not implemented!');
+});
+
+router.get('/:projectId/whiteboards/:whiteboardId/views/:viewId', (req, res) => {
+  // Get whiteboard view by id
+  res.send('Not implemented!');
+});
+
+router.delete('/:projectId/whiteboards/:whiteboardId/views/:viewId', (req, res) => {
+  // Delete whiteboard view by id
+  res.send('Not implemented!');
+});
+
+router.patch('/:projectId/whiteboards/:whiteboardId/views/:viewId', (req, res) => {
+  // Modify whiteboard view by id
+  res.send('Not implemented!');
+});
+
+router.put('/:projectId/whiteboards/:whiteboardId/views/:viewId', (req, res) => {
+  // Replace whiteboard view by id
+  res.send('Not implemented!');
+});
+
+module.exports = router;
