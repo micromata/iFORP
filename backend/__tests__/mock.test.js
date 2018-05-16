6603d05c463e454172f96ca539ce509cc2767aac
Nicolai Süper <n.sueper@micromata.de>
2018-05-17 00:23:13 +0200

    Add markup util +tests

--- backend/__tests__/mock.test.js
+++
@@ -1,3 +0,0 @@
-test('Application', () => {
-  expect(true);
-});
---
+++ backend/src/markup-util.ts
@@ -0,0 +1,56 @@
+import { Asset } from './orm/entity/Asset';
+import cheerio from 'cheerio';
+
+export function extractScriptAssets(markup: string): Array<Asset> {
+  const $ = cheerio.load(markup);
+  const scripts = $('script');
+  return scripts.toArray().map(el => {
+    const asset = mapElementToAsset(el);
+    asset.type = 'js';
+    return asset;
+  });
+}
+
+export function extractStyleAssets(markup: string): Array<Asset> {
+  const $ = cheerio.load(markup);
+  const inlineStyles = $('style');
+  const linkedStyles = $('link[rel="stylesheet"]');
+  return [...inlineStyles.toArray(), ...linkedStyles.toArray()].map(el => {
+    const asset = mapElementToAsset(el);
+    asset.type = 'css';
+    return asset;
+  });
+}
+
+function removeAssetsFromDocument($: CheerioStatic) {
+  $('link[rel="stylesheet"]').remove();
+  $('style').remove();
+  $('script').remove();
+}
+
+export function extractDocumentHead(markup: string): string {
+  const $ = cheerio.load(markup);
+  removeAssetsFromDocument($);
+  return $('head')
+    .html()
+    .trim();
+}
+
+export function extractDocumentBody(markup: string): string {
+  const $ = cheerio.load(markup);
+  removeAssetsFromDocument($);
+  return $('body')
+    .html()
+    .trim();
+}
+
+function mapElementToAsset(el: CheerioElement): Asset {
+  const asset = new Asset();
+  const styleSrc = el.attribs['src'] || el.attribs['href'];
+  if (styleSrc) {
+    asset.location = styleSrc;
+  } else {
+    asset.contents = el.firstChild.nodeValue.trim();
+  }
+  return asset;
+}
