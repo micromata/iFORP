8d105997618ba7688371638ed0651ef365c72e4c
Michael Kühnel <m.kuehnel@micromata.de>
2018-05-24 12:39:46 +0200

    Fix test setup

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,5 @@
-import express from 'express';
-import cors from 'cors';
+import * as express from 'express';
+import * as cors from 'cors';
 import * as bodyParser from 'body-parser';
 import libraryEndpoint from './routes/library';
 import projectsEndpoint from './routes/projects';
