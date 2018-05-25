f92fb5a69342d550ead56cf6c9f1a8bbd318b2e5
Michael Kühnel <m.kuehnel@micromata.de>
2018-05-25 10:17:22 +0200

    Add FIXME note regarding archer issue

--- frontend/src/app/pages/whiteboards/whiteboards.views.js
+++ frontend/src/app/pages/whiteboards/whiteboards.views.js
@@ -18,6 +18,13 @@ export const Views = ({views, onDeleteView, onRenameView, onAddView, projectId, 
 	};
 
 	return (
+		/**
+		 * FIXME:
+		 * Archers don’t destroy themselves when switching whiteboards.
+		 * So we got an error when going back from one whiteboard to another via the whiteboard switcher.
+		 * It seems that this is something we dont have control over.
+		 * See: https://github.com/pierpo/react-archer/blob/8bb53fc1d7c5b955db7b3f3cc41a246ee6ab47d4/src/ArcherContainer.js#L104
+		 */
 		<ArcherContainer strokeColor="grey">
 			<div className="row justify-content-md-center text-center">
 				{views &&
