d7bb5d31d4fe575a8607620ff8f8befcbe1605b8
Andy Bestvater <a.bestvater@micromata.de>
2018-08-16 16:58:06 +0200

    Set logo of app

--- frontend/src/pages/login/Login.js
+++ frontend/src/pages/login/Login.js
@@ -3,11 +3,15 @@ import injectSheet from 'react-jss';
 import { styles } from './Login.styles';
 import MicromataLogo from '../../assets/img/micromata.svg';
 import BBFLogo from '../../assets/img/BBF.png';
+import Logo from '../../assets/img/iForp.png';
 
 const Login = ({ classes }) => (
   <div className={classes.Login}>
     <main>
       <div className="grid">
+        <div className="logo">
+          <img src={Logo} alt="iForp" />
+        </div>
       </div>
     </main>
     <footer>
